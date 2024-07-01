81c2f6800a968a3b2dabeb900cd3c1c0c2cd72e3
Moeein Aali <moeeeinaali@gmail.com>
2024-07-02 02:42:29 +0330

    Add Profile Layout

---
+++ KingOfQuizes/app/src/main/java/com/moeein/kingofquizes/profile.java
@@ -0,0 +1,25 @@
+package com.moeein.kingofquizes;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_profile);
+    }
+}
