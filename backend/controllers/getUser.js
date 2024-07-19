746e00f44f3d8cbad247176ce492d0a0cfdb9956
Moeein Aali <moeeeinaali@gmail.com>
2024-07-19 23:59:37 +0330

    Add getUser with email

--- backend/controllers/getUser.js
+++ backend/controllers/getUser.js
@@ -1,9 +1,19 @@
 module.exports = (fastify) => {
     return async (req, res) => {
         const username = req.query.username;
-        const QUERY = `SELECT * FROM users WHERE username = $1;`;
+        const email = req.query.email;
         try {
-            const result = await fastify.pg.query(QUERY, [username]);
+            let QUERY = ""
+            let input = ""
+            if (username) {
+                QUERY = `SELECT * FROM users WHERE username = $1;`;
+                input = username;
+            } else if (email) {
+                QUERY = `SELECT * FROM users WHERE email = $1;`;
+                input = email;
+            }
+
+            const result = await fastify.pg.query(QUERY, [input]);
             res.send(result.rows[0]);
         } catch (err) {
             console.error("Database query error: ", err);
