77373765c6010c8944de841f83f97bddb4ca60bd
Moeein Aali <moeeeinaali@gmail.com>
2024-07-19 23:20:48 +0330

    Add /users and /user?username API

--- backend/controllers/getAllUsers.js
+++ backend/controllers/getAllUsers.js
@@ -1,6 +1,12 @@
 module.exports = (fastify) => {
     return async (req, res) => {
-        const result = await fastify.pg.query("SELECT * FROM users");
-        res.send(result.rows);
+        const QUERY = `SELECT * FROM users;`;
+        try {
+            const result = await fastify.pg.query(QUERY);
+            res.send(result.rows);
+        } catch (err) {
+            console.error("Database query error: ", err);
+            res.status(500).send({ error: "Database query error" });
+        }
     };
 };
--- backend/controllers/getUser.js
+++ backend/controllers/getUser.js
@@ -1,9 +1,13 @@
 module.exports = (fastify) => {
     return async (req, res) => {
-        const username = req.body.username;
-        await fastify.pg.query("INSERT INTO users (username) VALUES ($1)", [
-            username,
-        ]);
-        res.send("ok");
+        const username = req.query.username;
+        const QUERY = `SELECT * FROM users WHERE username = $1;`;
+        try {
+            const result = await fastify.pg.query(QUERY, [username]);
+            res.send(result.rows[0]);
+        } catch (err) {
+            console.error("Database query error: ", err);
+            res.status(500).send({ error: "Database query error" });
+        }
     };
 };
