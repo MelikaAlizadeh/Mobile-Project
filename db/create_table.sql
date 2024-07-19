469c0b3590120b4106230253d9a1d8c81c942f55
Moeein Aali <moeeeinaali@gmail.com>
2024-07-19 21:47:33 +0330

    Fix Create Tables

--- db/create_table.sql
+++ db/create_table.sql
@@ -9,26 +9,34 @@ CREATE TABLE users
 
 CREATE TABLE questions
 (
-    q_id SERIAL PRIMARY KEY,
-    question TEXT,
-    option1 TEXT,
-    option2 TEXT,
-    option3 TEXT,
-    option4 TEXT,
+    q_id           SERIAL PRIMARY KEY,
+    question       TEXT,
+    option1        TEXT,
+    option2        TEXT,
+    option3        TEXT,
+    option4        TEXT,
     correct_answer INT
 );
 
-CREATE TABLE general_qs () INHERITS (questions);
-CREATE TABLE sports_qs () INHERITS (questions);
-CREATE TABLE art_qs () INHERITS (questions);
-CREATE TABLE programming_qs () INHERITS(questions);
+CREATE TABLE general_qs
+(
+) INHERITS (questions);
+CREATE TABLE sports_qs
+(
+) INHERITS (questions);
+CREATE TABLE art_qs
+(
+) INHERITS (questions);
+CREATE TABLE programming_qs
+(
+) INHERITS (questions);
 
 CREATE TABLE teacher
 (
     teacher_id SERIAL PRIMARY KEY,
-    f_name TEXT,
-    l_name TEXT,
-    ph_number VARCHAR(11),
-    price DECIMAL,
-    score FLOAT
+    f_name     TEXT,
+    l_name     TEXT,
+    ph_number  VARCHAR(11),
+    price      DECIMAL,
+    score      FLOAT
 );
