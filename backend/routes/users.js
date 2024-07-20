d235faf911a920af6828429b67c1c133a092cfc4
Moeein Aali <moeeeinaali@gmail.com>
2024-07-20 08:49:37 +0330

    Add GET /topUsers

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,9 +3,11 @@ module.exports = (fastify) => {
     const getUser = require("../controllers/getUser.js")(fastify);
     const createUser = require("../controllers/createUser")(fastify);
     const updateScore = require("../controllers/updateScore")(fastify);
+    const getTops = require("../controllers/topUsers.js")(fastify);
 
     fastify.get("/getAllUsers", getAllUsers);
     fastify.get("/getUser", getUser);
+    fastify.get("/getTops", getTops);
     fastify.post("/createUser", createUser)
     fastify.post("/updateScore", updateScore)
 };
