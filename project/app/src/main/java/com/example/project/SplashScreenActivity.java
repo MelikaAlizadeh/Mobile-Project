485e6bf3193bae5773bf786e624a495688d88d28
Melika Alizadeh <Melikaalizadeh7@gmail.com>
2024-08-01 21:50:23 +0330

    move on onboard

--- project/app/src/main/java/com/example/project/SplashScreenActivity.java
+++ project/app/src/main/java/com/example/project/SplashScreenActivity.java
@@ -1,6 +1,7 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -12,13 +13,31 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent splash = new Intent(SplashScreenActivity.this, OnBoard1.class);
-                startActivity(splash);
-                finish();
-            }
-        }, 3000);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        String firstTime = sharedPreferences.getString("isFirstRun", "");
+
+        if (firstTime.equals("Yes")) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashScreenActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 3000);
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("isFirstRun", "Yes");
+                    editor.apply();
+                    Intent splash = new Intent(SplashScreenActivity.this, OnBoard1.class);
+                    startActivity(splash);
+                    finish();
+                }
+            }, 3000);
+        }
     }
 }
